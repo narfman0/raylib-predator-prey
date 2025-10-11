c7d81f418d4bdf21b8f32ae5e45279671f031285
Jon Robison <narfman0@blastedstudios.com>
2025-10-10 23:11:41 -0400

    move entities around

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,13 +2,28 @@
 #include <ctime>
 
 #include "raylib.h"
+#include <raymath.h>
+
+int gridSize = 50;
+float speed = 2.0f;
+float maxEnergy = 10.0f;
 
 enum EntityType {PREDATOR, PREY};
 struct Entity{
     Vector3 position;
     Vector3 velocity;
     EntityType type;
+    float spawnTime;
+    float energy;
 };
+
+inline float randRange(float min, float max){
+    return min + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(max-min)));
+}
+
+Entity spawnEntity(Vector3 pos, EntityType type){
+    return Entity(Vector3{pos}, Vector3{randRange(-speed, speed), 0, randRange(-speed, speed)}, type);
+}
 
 int main(void)
 {
@@ -29,18 +44,41 @@ int main(void)
     DisableCursor();
     SetTargetFPS(60);
 
-    int gridSize = 100;
+    float gridSizeF = (float)gridSize;
+    float gridSizeHalfF = (float)gridSize/2.f;
     std::vector<Entity> entities;
-    for(int i=0; i<100; i++){
-        Vector3 pos{rand() % gridSize - gridSize/2, 0, rand() % gridSize - gridSize/2};
-        Vector3 vel{rand() % 3, 0, rand() % 3};
+    for(int i=0; i<gridSize; i++){
+        Vector3 pos{randRange(-gridSizeHalfF, gridSizeHalfF), 0, randRange(-gridSizeHalfF, gridSizeHalfF)};
+        Vector3 vel{randRange(-speed, speed), 0, randRange(-speed, speed)};
         EntityType type = i % 4 == 0 ? EntityType::PREDATOR : EntityType::PREY;
         entities.emplace_back(pos, vel, type);
     }
 
     while (!WindowShouldClose())
     {
-        // TODO update pos
+        float dt = GetFrameTime();
+        for(std::vector<Entity>::iterator it = entities.begin(); it != entities.end(); it++){
+            Entity& entity = *it;
+            entity.position = Vector3Add(entity.position, entity.velocity*dt);
+            if(entity.position.x > gridSizeHalfF){
+                entity.position.x = gridSizeHalfF;
+                entity.velocity.x *= -1;
+            }
+            if(entity.position.z > gridSizeHalfF){
+                entity.position.z = gridSizeHalfF;
+                entity.velocity.z *= -1;
+            }
+            if(entity.position.x < -gridSizeHalfF){
+                entity.position.x = -gridSizeHalfF;
+                entity.velocity.x *= -1;
+            }
+            if(entity.position.z < -gridSizeHalfF){
+                entity.position.z = -gridSizeHalfF;
+                entity.velocity.z *= -1;
+            }
+            // TODO get closest non-type
+            // TODO spawn offspring of same type
+        }
 
 
         UpdateCamera(&camera, CAMERA_FIRST_PERSON);
@@ -48,8 +86,8 @@ int main(void)
             ClearBackground(RAYWHITE);
             BeginMode3D(camera);
                 for(Entity entity : entities){
-                    DrawCube(entity.position, 2.0f, 2.0f, 2.0f, entity.type == EntityType::PREDATOR ? RED : GREEN);
-                    DrawCubeWires(entity.position, 2.0f, 2.0f, 2.0f, entity.type == EntityType::PREDATOR ? MAROON : LIME);
+                    DrawCube(entity.position, 1.0f, 1.0f, 1.0f, entity.type == EntityType::PREDATOR ? RED : GREEN);
+                    DrawCubeWires(entity.position, 1.0f, 1.0f, 1.0f, entity.type == EntityType::PREDATOR ? MAROON : LIME);
                 }
                 DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
                 DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
